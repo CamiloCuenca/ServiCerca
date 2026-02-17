72502b4ffbe955a244de37c66fc8e6b04e97f2aa
DiEGO <jimenezdiegoalexander1@gmail.com>
2026-02-17 18:37:13 -0500

    Inicio de la creación de Perfil
    Se inicio la creación de la pestaña de mi perfil

---
+++ app/src/main/java/com/servicerca/app/ui/profile/ProfileScreen.kt
@@ -0,0 +1,202 @@
+package com.servicerca.app.ui.profile
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.servicerca.app.R
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.filled.AddLocation
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.core.graphics.translationMatrix
+
+@Composable
+fun ProfileScreen(
+    onNavigateToHome: () -> Unit,
+){
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize()
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .verticalScroll(rememberScrollState())
+                .padding(horizontal = 24.dp),
+            verticalArrangement = Arrangement.spacedBy(20.dp)
+        ) {
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 16.dp)
+            ) {
+
+
+                IconButton(
+                    onClick = onNavigateToHome, // TODO @CamiloCuenca cambiar para que recuerde cual fue lapantalla anteriro (ver guias del profe)
+                    modifier = Modifier.align(Alignment.CenterStart)
+                ) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = "Volver"
+                    )
+                }
+
+
+                Text(
+                    text = stringResource(R.string.profile_text),
+                    style = MaterialTheme.typography.headlineSmall,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.align(Alignment.Center)
+                )
+
+                IconButton(
+                    onClick = onNavigateToHome, // TODO @CamiloCuenca cambiar para que recuerde cual fue lapantalla anteriro (ver guias del profe)
+                    modifier = Modifier.align(Alignment.CenterEnd)
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Settings,
+                        contentDescription = "Configuración"
+                    )
+                }
+            }
+
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Box(
+                    contentAlignment = Alignment.BottomEnd, // El icono va en la esquina inferior derecha
+                    modifier = Modifier.size(150.dp)
+                ) {
+                    // Imagen de perfil
+                    Card(
+                        shape = CircleShape,
+                        colors = CardDefaults.cardColors(
+                            Color.White
+                        ),
+                        modifier = Modifier
+                            .size(150.dp)
+                            .shadow(
+                                elevation = 20.dp,
+                                shape = CircleShape,
+                                ambientColor = Color.Cyan,
+                                spotColor = Color.Cyan
+                            )
+                    )
+                    {
+                        Image(painter = painterResource(id = R.drawable.logo_profile), contentDescription = null,
+                            modifier = Modifier
+                                .size(250.dp)
+                                .shadow(
+                                    elevation = 16.dp,
+                                    shape = CircleShape,
+                                    ambientColor = Color.Cyan,
+                                    spotColor = Color.Cyan
+                                )
+                        )
+                    }
+
+
+                    // Icono para cambiar foto
+                    Icon(
+                        imageVector = Icons.Default.Edit,
+                        contentDescription = "Cambiar foto",
+                        tint = Color.White, //Color del lapiz
+                        modifier = Modifier
+                            .size(40.dp)
+                            .background(Color.Black, CircleShape) //Color del circulo
+                            .padding(8.dp)
+                            .clickable {
+                                // Aca iria la acción para cambiar la foto que no sé como hacer :D
+                            }
+                    )
+                }
+            }
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    Text(
+                        text = stringResource(R.string.name_user),
+                        style = MaterialTheme.typography.headlineSmall,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.align(Alignment.Center)
+                    )
+                }
+            Box(
+                modifier = Modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.LocationOn,
+                        contentDescription = "Ubicación",
+                        tint = Color.Cyan,
+                        modifier = Modifier.size(25.dp)
+                    )
+
+                    Spacer(modifier = Modifier.width(8.dp))
+
+                    Text(
+                        text = stringResource(R.string.lacation_profile),
+                        style = MaterialTheme.typography.bodyLarge,
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+
+        }
+        }
+    }
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun ProfileScreenPreview() {
+    ProfileScreen( onNavigateToHome = {})
+}
