a021e1e8aaecd6ff410de5473129c7134e813ead
Brandon Montealegre <brandonmontealegre15@gmail.com>
2026-02-19 19:27:00 -0500

    Se esta creando el screen servicio

--- app/src/main/java/com/servicerca/app/ui/services/detail/DetailServiceScreen.kt
+++ app/src/main/java/com/servicerca/app/ui/services/detail/DetailServiceScreen.kt
@@ -1,2 +1,167 @@
 package com.servicerca.app.ui.services.detail
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.servicerca.app.core.components.button.PrimaryButton
+import com.servicerca.app.core.components.input.AppPasswordField
+import com.servicerca.app.core.components.input.AppTextField
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import com.servicerca.app.R
+import com.servicerca.app.core.components.button.SocialButton
+
+@Composable
+fun DetailServiceScreen(
+
+
+
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(vertical = 24.dp)
+
+
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth()
+                .weight(1f) // 👈 esto empuja el contenido de abajo hacia el final
+
+        ) {
+            Box(
+                modifier = Modifier.fillMaxWidth().height(300.dp)
+            ){
+                Image(
+                    painter = painterResource(id = R.drawable.service),
+                    contentDescription = "service",
+                    contentScale = ContentScale.Crop, // importante para que llene bien
+                    modifier = Modifier.fillMaxSize()
+                )
+
+                IconButton(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier.align(Alignment.TopStart)
+                        .padding(8.dp)
+                        .background(
+                            color = Color.Black.copy(alpha = 0.5f),
+                            shape = CircleShape
+                        )
+
+
+                ) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = "Volver",
+                        tint = Color.White
+                    )
+
+                }
+
+                IconButton(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier.align(Alignment.TopEnd)
+                        .padding(8.dp)
+                        .background(
+                            color = Color.Black.copy(alpha = 0.5f),
+                            shape = CircleShape
+                        )
+
+
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Share,
+                        contentDescription = "Volver",
+                        tint = Color.White
+                    )
+
+
+
+                }
+
+
+
+            }
+
+
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .clip(RoundedCornerShape(16.dp))
+                    .border(
+                        width = 1.dp
+                        ,color = MaterialTheme.colorScheme.primary,
+                        shape = RoundedCornerShape(16.dp)
+                    )
+
+            ) {
+            }
+
+
+
+
+
+        }
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 8.dp)
+        ) {
+            OutlinedButton(
+                onClick = {},
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(16.dp)
+            ) {
+                Text("Solicitar Servicio")
+            }
+        }
+
+
+
+
+    }
+}
+
+@Preview (showBackground = true , showSystemUi = true)
+@Composable
+fun DetailServiceScreenPreview() {
+    DetailServiceScreen()
+}
