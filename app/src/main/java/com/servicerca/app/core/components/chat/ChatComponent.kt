db09a98f5fba3c9494c4e80818fd797f74616116
FedeAlvarezzz <federico.alvarezm@uqvirtual.edu.co>
2026-03-24 20:37:41 -0500

    ajustes minimos en ChatComponent.kt

--- app/src/main/java/com/servicerca/app/core/components/chat/ChatComponent.kt
+++ app/src/main/java/com/servicerca/app/core/components/chat/ChatComponent.kt
@@ -2,13 +2,11 @@ package com.servicerca.app.core.components.chat
 
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -27,7 +25,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
