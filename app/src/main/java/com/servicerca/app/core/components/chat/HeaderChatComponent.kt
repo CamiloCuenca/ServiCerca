65d27f0a4f19c6fd51b04378c28bd25547871d8c
FedeAlvarezzz <federico.alvarezm@uqvirtual.edu.co>
2026-03-24 20:37:13 -0500

    ajustes minimos en HeaderChatComponent.kt

--- app/src/main/java/com/servicerca/app/core/components/chat/HeaderChatComponent.kt
+++ app/src/main/java/com/servicerca/app/core/components/chat/HeaderChatComponent.kt
@@ -2,7 +2,6 @@ package com.servicerca.app.core.components.chat
 
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -25,7 +24,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.HorizontalAlignmentLine
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
