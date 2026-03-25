843ec8ce19b260abdae18ee4eb4dadb8a3722b84
FedeAlvarezzz <federico.alvarezm@uqvirtual.edu.co>
2026-03-24 20:36:28 -0500

    ajustes minimos en HeaderChatComponent.kt

--- app/src/main/java/com/servicerca/app/core/components/chat/HeaderChatComponent.kt
+++ app/src/main/java/com/servicerca/app/core/components/chat/HeaderChatComponent.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -14,6 +15,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -23,6 +25,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.HorizontalAlignmentLine
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -37,7 +40,7 @@ fun HeaderChatComponent(
     onBack : () -> Unit = {}
 ) {
 
-    Box (
+    Column (
         modifier = Modifier
             .fillMaxWidth()
     ) {
@@ -108,6 +111,10 @@ fun HeaderChatComponent(
                 )
             }
         }
+
+        Spacer(modifier = Modifier.height(6.dp))
+        HorizontalDivider(thickness = 1.dp, color = MaterialTheme.colorScheme.outline)
+
     }
 }
 
