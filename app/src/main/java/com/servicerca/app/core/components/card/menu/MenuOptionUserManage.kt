8359a14aa1f308306c4eadd9c6b81c792272adad
FedeAlvarezzz <federico.alvarezm@uqvirtual.edu.co>
2026-03-27 19:29:05 -0500

    componente reutilizable para cada opcion del menu de la gestion de usuarios

--- app/src/main/java/com/servicerca/app/core/components/card/menu/MenuOptionUserManage.kt
+++ app/src/main/java/com/servicerca/app/core/components/card/menu/MenuOptionUserManage.kt
@@ -10,13 +10,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.contextmenu.modifier.appendTextContextMenuComponents
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ChevronRight
-import androidx.compose.material.icons.filled.Group
 import androidx.compose.material.icons.filled.RemoveRedEye
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
