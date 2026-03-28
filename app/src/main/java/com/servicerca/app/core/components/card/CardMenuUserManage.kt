05e6dbd82aa855ed75d24625a8d6632e54735bec
FedeAlvarezzz <federico.alvarezm@uqvirtual.edu.co>
2026-03-27 19:31:02 -0500

    card principal para mostrar el menu de las opciones disponibles para gestionar cada usuario

---
+++ app/src/main/java/com/servicerca/app/core/components/card/CardMenuUserManage.kt
@@ -0,0 +1,59 @@
+package com.servicerca.app.core.components.card
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Block
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.RemoveRedEye
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.servicerca.app.R
+import com.servicerca.app.core.components.card.menu.MenuOptionUserManage
+
+@Composable
+fun CardMenuUserManage (){
+
+    ElevatedCard(
+        modifier = Modifier
+            .fillMaxWidth(1f),
+        shape = RoundedCornerShape(16.dp),
+        elevation = CardDefaults.cardElevation(4.dp),
+
+    ) {
+        MenuOptionUserManage(
+            text = stringResource(R.string.menu_option_see_profile),
+            icon = Icons.Default.RemoveRedEye,
+            onClick = {}
+
+        )
+
+        MenuOptionUserManage(
+            text = stringResource(R.string.menu_option_suspend_profile),
+            icon = Icons.Default.Block,
+            onClick = {}
+
+        )
+
+        HorizontalDivider()
+
+        MenuOptionUserManage(
+            text = stringResource(R.string.menu_option_delete_profile),
+            icon = Icons.Default.Delete,
+            onClick = {}
+
+        )
+    }
+}
+
+@Preview(showBackground = false)
+@Composable
+fun CardMenuUserManagePreview(){
+    CardMenuUserManage()
+}
