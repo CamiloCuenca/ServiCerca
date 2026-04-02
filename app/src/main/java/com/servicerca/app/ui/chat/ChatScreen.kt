f8e3bd3c9b69bf8f8d4cf7d273d29c2f476e58ec
FedeAlvarezzz <federico.alvarezm@uqvirtual.edu.co>
2026-04-02 01:05:52 -0500

    Implementacion del ViewModel para ChatScreen.kt

--- app/src/main/java/com/servicerca/app/ui/chat/ChatScreen.kt
+++ app/src/main/java/com/servicerca/app/ui/chat/ChatScreen.kt
@@ -5,23 +5,32 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.servicerca.app.R
 import com.servicerca.app.core.components.chat.HeaderChatComponent
 import com.servicerca.app.core.components.chat.MessageBubble
 import com.servicerca.app.core.components.chat.SendMessageChatComponent
 
 @Composable
-fun ChatScreen(){
+fun ChatScreen(
+    viewModel: ChatScreenViewModel = viewModel(),
+    onBack: () -> Unit = {}
+){
+
+    val uiState by viewModel.uiState.collectAsState()
 
     Column {
         HeaderChatComponent(
             imageProfile = R.drawable.foto_perfil,
             nameProfile = stringResource(R.string.name_profile),
-            onlineStatus = true
+            onlineStatus = uiState.isOnline,
+            onBack = onBack
         )
 
         Column (
@@ -30,105 +39,21 @@ fun ChatScreen(){
                 .weight(1f)
                 .verticalScroll(rememberScrollState())
         ){
-            MessageBubble(
-                message = "que se dice gran hijueputa",
-                time = "7:15 PM",
-                isMine = false,
-                imageProfile = R.drawable.foto_perfil
-            )
-
-            MessageBubble(
-                message = "entonces que gonorrea, por que me insulta?",
-                time = "7:18 PM",
-                isMine = true
-            )
-
-            MessageBubble(
-                message = "perdon, es que me confundi de chat",
-                time = "7:19 PM",
-                isMine = false,
-                imageProfile = R.drawable.foto_perfil
-            )
-
-            MessageBubble(
-                message = "ah bueno, no hay problema, pero se mas respetuoso con las personas",
-                time = "7:20 PM",
-                isMine = true
-            )
-
-            MessageBubble(
-                message = "si, lo siento, te quiero mucho",
-                time = "7:21 PM",
-                isMine = false,
-                imageProfile = R.drawable.foto_perfil
-            )
-            MessageBubble(
-                message = "que se dice gran hijueputa",
-                time = "7:15 PM",
-                isMine = false,
-                imageProfile = R.drawable.foto_perfil
-            )
-
-            MessageBubble(
-                message = "entonces que gonorrea, por que me insulta?",
-                time = "7:18 PM",
-                isMine = true
-            )
-
-            MessageBubble(
-                message = "perdon, es que me confundi de chat",
-                time = "7:19 PM",
-                isMine = false,
-                imageProfile = R.drawable.foto_perfil
-            )
-
-            MessageBubble(
-                message = "ah bueno, no hay problema, pero se mas respetuoso con las personas",
-                time = "7:20 PM",
-                isMine = true
-            )
-
-            MessageBubble(
-                message = "si, lo siento, te quiero mucho",
-                time = "7:21 PM",
-                isMine = false,
-                imageProfile = R.drawable.foto_perfil
-            )
-            MessageBubble(
-                message = "que se dice gran hijueputa",
-                time = "7:15 PM",
-                isMine = false,
-                imageProfile = R.drawable.foto_perfil
-            )
-
-            MessageBubble(
-                message = "entonces que gonorrea, por que me insulta?",
-                time = "7:18 PM",
-                isMine = true
-            )
-
-            MessageBubble(
-                message = "perdon, es que me confundi de chat",
-                time = "7:19 PM",
-                isMine = false,
-                imageProfile = R.drawable.foto_perfil
-            )
-
-            MessageBubble(
-                message = "ah bueno, no hay problema, pero se mas respetuoso con las personas",
-                time = "7:20 PM",
-                isMine = true
-            )
-
-            MessageBubble(
-                message = "si, lo siento, te quiero mucho",
-                time = "7:21 PM",
-                isMine = false,
-                imageProfile = R.drawable.foto_perfil
-            )
+            uiState.messages.forEach { message ->
+                MessageBubble(
+                    message = message.message,
+                    time = message.time,
+                    isMine = message.isMine,
+                    imageProfile = message.imageProfile
+                )
+            }
         }
 
-        SendMessageChatComponent()
+        SendMessageChatComponent(
+            message = uiState.currentMessage,
+            onMessageChange = { viewModel.onMessageChange(it) },
+            onSendMessage = { viewModel.sendMessage() }
+        )
 
     }
 }
