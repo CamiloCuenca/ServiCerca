69751a4bb1340371e6eb9dc42343c49e9a49501e
FedeAlvarezzz <federico.alvarezm@uqvirtual.edu.co>
2026-04-19 02:11:43 -0500

    feat: update HeaderChatComponent to use AsyncImage for profile pictures and change imageProfile type to String

--- app/src/main/java/com/servicerca/app/core/components/chat/HeaderChatComponent.kt
+++ app/src/main/java/com/servicerca/app/core/components/chat/HeaderChatComponent.kt
@@ -1,7 +1,5 @@
 package com.servicerca.app.core.components.chat
 
-import androidx.annotation.DrawableRes
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -24,15 +22,17 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
 import com.servicerca.app.R
 
 @Composable
 fun HeaderChatComponent(
-    @DrawableRes imageProfile : Int,
+    imageProfile : String,
     nameProfile : String = "",
     onlineStatus : Boolean = false,
     onBack : () -> Unit = {}
@@ -55,13 +55,16 @@ fun HeaderChatComponent(
             )
         }
 
-        Image(
-            painter = painterResource(id = imageProfile),
+        AsyncImage(
+            model = imageProfile,
             contentDescription = "Foto de perfil",
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
-                .align(Alignment.CenterVertically)
+                .align(Alignment.CenterVertically),
+            contentScale = ContentScale.Crop,
+            placeholder = painterResource(R.drawable.primo_de_juan_camilo),
+            error = painterResource(R.drawable.primo_de_juan_camilo)
         )
 
         Column(
@@ -121,7 +124,7 @@ fun HeaderChatComponent(
 @Preview(showBackground = true)
 fun HeaderChatComponentPreview() {
     HeaderChatComponent(
-        imageProfile = R.drawable.primo_de_juan_camilo,
+        imageProfile = "",
         nameProfile = "Deiver Bonano Cuenca",
         onlineStatus = true
     )
